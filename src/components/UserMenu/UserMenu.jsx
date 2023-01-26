77654b59a140ec8045aafa2cff339650e67ae42f
Ihor Smyk <ismyk256@gmail.com>
2023-01-26 15:14:20 +0200

    upd

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from 'redux/operations';
+import s from './UserMenu.module.css';
+
+export const UserMenu = () => {
+  const email = useSelector(state => state.auth.email);
+  const dispatch = useDispatch();
+
+  return (
+    <div className={s.userMenu}>
+      <p className={s.userEmail}>{email}</p>
+      <button
+        type="button"
+        onClick={() => dispatch(logout())}
+        className={s.link}
+      >
+        Sign Out
+      </button>
+    </div>
+  );
+};
