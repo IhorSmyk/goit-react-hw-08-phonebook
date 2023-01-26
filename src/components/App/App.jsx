907fa748d15c4d8f7e45b8ea43be05041763c01b
Ihor Smyk <ismyk256@gmail.com>
2023-01-26 14:03:31 +0200

    upd

---
+++ src/components/App/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { getUser } from 'redux/operations';
+import { AppBar } from './AppBar/AppBar';
+import { Register } from './Register/Register';
+import { Login } from './Login/Login';
+import { Contacts } from './Contacts/Contacts';
+import 'react-toastify/dist/ReactToastify.css';
+import s from './App.module.css';
+
+export const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <AppBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<div className={s.title}>Welcome to our phonebook!</div>}
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+      <ToastContainer autoClose={2000} />
+    </div>
+  );
+};
