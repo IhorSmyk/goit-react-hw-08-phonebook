8198f35295112990b3c9b12c72c7bae4fd55e3f2
Ihor Smyk <ismyk256@gmail.com>
2023-01-27 03:09:23 +0200

    upd

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -10,61 +10,62 @@ const authSlice = createSlice({
     isAuth: false,
     isLoading: false,
   },
-  extraReducers: {
-    [getUser.pending](state) {
-      state.isLoading = true;
-    },
-    [getUser.fulfilled](state, action) {
-      state.name = action.payload.name;
-      state.email = action.payload.email;
-      state.isAuth = true;
-      state.isLoading = false;
-    },
-    [getUser.rejected](state) {
-      state.token = null;
-      state.name = '';
-      state.email = '';
-      state.isLoading = false;
-    },
-    [register.pending](state) {
-      state.isLoading = true;
-    },
-    [register.fulfilled](state, action) {
-      state.name = action.payload.user.name;
-      state.email = action.payload.user.email;
-      state.token = action.payload.token;
-      state.isAuth = true;
-      state.isLoading = false;
-    },
-    [register.rejected](state) {
-      state.isLoading = false;
-    },
-    [login.pending](state) {
-      state.isLoading = true;
-    },
-    [login.fulfilled](state, action) {
-      state.name = action.payload.user.name;
-      state.email = action.payload.user.email;
-      state.token = action.payload.token;
-      state.isAuth = true;
-      state.isLoading = false;
-    },
-    [login.rejected](state) {
-      state.isLoading = false;
-    },
-    [logout.pending](state) {
-      state.isLoading = true;
-    },
-    [logout.fulfilled](state, action) {
-      state.name = '';
-      state.email = '';
-      state.token = '';
-      state.isAuth = false;
-      state.isLoading = false;
-    },
-    [logout.rejected](state) {
-      state.isLoading = false;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(getUser.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isAuth = true;
+        state.name = action.payload.name;
+        state.email = action.payload.email;
+      })
+      .addCase(getUser.rejected, state => {
+        state.isLoading = false;
+        state.token = null;
+        state.name = '';
+        state.email = '';
+      })
+      .addCase(register.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isAuth = true;
+        state.name = action.payload.user.name;
+        state.email = action.payload.user.email;
+        state.token = action.payload.token;
+      })
+      .addCase(register.rejected, state => {
+        state.isLoading = false;
+      })
+      .addCase(login.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.name = action.payload.user.name;
+        state.email = action.payload.user.email;
+        state.token = action.payload.token;
+        state.isAuth = true;
+        state.isLoading = false;
+      })
+      .addCase(login.rejected, state => {
+        state.isLoading = false;
+      })
+      .addCase(logout.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(logout.fulfilled, state => {
+        state.name = '';
+        state.email = '';
+        state.token = '';
+        state.isAuth = false;
+        state.isLoading = false;
+      })
+      .addCase(logout.rejected, state => {
+        state.isLoading = false;
+      });
   },
 });
 
